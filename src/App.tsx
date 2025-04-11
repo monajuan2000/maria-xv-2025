4c16fa563fc1f9bd4bd3c5b5327ea7b3e9b20a90
Juan Esteban Mona Bermudez <monajuan2000@gmail.com>
2025-04-11 00:46:05 -0500

    feat: add two important target dates for countdowns (event and delivery date)

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import backgroundVideo from './assets/videos/PC-main-wallpaper-1.mp4';
 //import Home from './pages/Home';
 import './styles/app.css';
 import TextTesting from './components/testing/TextTesting';
+import CountdownComponentV1 from './components/countdown/CountdownComponentV1';
+import CountdownComponentV2 from './components/countdown/CountdownComponentV2';
 
 function App() {
   return (
@@ -16,6 +18,14 @@ function App() {
 
         <section className="p-4">
           <TextTesting />
+        </section>
+
+        <section className="p-4">
+          <CountdownComponentV2 />
+        </section>
+
+        <section className="p-4">
+          <CountdownComponentV1 />
         </section>
 
 
