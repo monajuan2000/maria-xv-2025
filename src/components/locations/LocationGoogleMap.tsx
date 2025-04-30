45ea26828f0a6dc5143f5e95be23546c2262ac02
Juan Esteban Mona Bermudez <monajuan2000@gmail.com>
2025-04-29 21:06:25 -0500

    style: update maps with circular design for better visual integration

--- src/components/locations/LocationGoogleMap.tsx
+++ src/components/locations/LocationGoogleMap.tsx
@@ -13,17 +13,40 @@ export default function LocationGoogleMap() {
                 </span>
             </section>
             <br />
-            <section className="w-full h-[400px]">
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d438024.1620822586!2d-74.49809160931113!3d4.576945351412021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9b500305e907%3A0x5fa67e144bb766b6!2sSede%20Social%20La%20Salina!5e0!3m2!1sen!2sco!4v1744346388810!5m2!1sen!2sco"
-                    width="100%"
-                    height="100%"
-                    style={{ border: 0 }}
-                    //allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
+            <section
+
+                style={{
+                    width: '100vw',
+                    height: '40vh',
+                    margin: 0,
+                    padding: 0,
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <div
+                    style={{
+                        width: '90vmin',
+                        height: '90vmin',
+                        borderRadius: '50%',
+                        overflow: 'hidden',
+                        border: '4px solid white',
+                    }}
+                >
+
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.41354871314!2d-74.11504992396571!3d4.697993895277024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9b500305e907%3A0x5fa67e144bb766b6!2sSede%20Social%20La%20Salina!5e0!3m2!1sen!2sco!4v1745978456664!5m2!1sen!2sco"
+                        width="100%"
+                        height="100%"
+                        style={{ border: 0 }}
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                        allowFullScreen
+                    ></iframe>
+                </div>
             </section>
+
 
             <br />
             <section className="m-4 flex justify-center">
--- src/components/locations/OpenStreetMapLeaflet.tsx
+++ src/components/locations/OpenStreetMapLeaflet.tsx
@@ -23,3 +23,55 @@ export default function OpenStreetMapLeaflet() {
         </MapContainer>
     );
 }
+
+
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import { LatLngExpression } from 'leaflet';
+// import 'leaflet/dist/leaflet.css';
+
+// export default function OpenStreetMapLeaflet() {
+//     const center: LatLngExpression = [4.576945, -74.498091];
+
+//     return (
+//         <div
+//             style={{
+//                 width: '100vw',
+//                 height: '100vh',
+//                 margin: '0',
+//                 padding: '0',
+//                 position: 'relative',
+//                 display: 'flex',
+//                 alignItems: 'center',
+//                 justifyContent: 'center',
+//             }}
+//         >
+//             <div
+//                 style={{
+//                     width: '90vmin',
+//                     height: '90vmin',
+//                     borderRadius: '50%',
+//                     overflow: 'hidden',
+//                     border: '4px solid white',
+//                 }}
+//             >
+//                 <MapContainer
+//                     center={center}
+//                     zoom={15}
+//                     style={{ height: '100%', width: '100%' }}
+//                     scrollWheelZoom={true}
+//                     doubleClickZoom={true}
+//                     dragging={true}
+//                     zoomControl={true}
+//                 >
+//                     <TileLayer
+//                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//                         attribution='&copy; OpenStreetMap contributors'
+//                     />
+//                     <Marker position={center}>
+//                         <Popup>Colombia</Popup>
+//                     </Marker>
+//                 </MapContainer>
+//             </div>
+//         </div>
+//     );
+// }
