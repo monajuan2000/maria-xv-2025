3ab0b51477bc6aedae00eafb030913584b3175f1
Juan Esteban Mona Bermudez <monajuan2000@gmail.com>
2025-05-11 16:22:11 -0500

    style: update layout and typography for better text presentation

--- src/components/header/MisQuince.tsx
+++ src/components/header/MisQuince.tsx
@@ -1,13 +1,13 @@
-export default function MisQuince() {
-    return (
-        <h1 className="relative inline-block font-decorative text-5xl font-semibold text-gray-200 italic text-italic-custom">
-            {/* Primer Span - Efecto de brillo animado */}
-            <span className="absolute inset-0 text-transparent bg-clip-text bg-gradient-to-r from-[#c0c0c0] via-[#e0e0e0] to-[#c0c0c0] bg-[length:200%_auto] animate-shimmer">
-                Mis Quince Años
-            </span>
+// export default function MisQuince() {
+//     return (
+//         <h1 className="relative inline-block font-decorative text-5xl font-semibold text-gray-200 italic text-italic-custom">
+//             {/* Primer Span - Efecto de brillo animado */}
+//             <span className="absolute inset-0 text-transparent bg-clip-text bg-gradient-to-r from-[#c0c0c0] via-[#e0e0e0] to-[#c0c0c0] bg-[length:200%_auto] animate-shimmer">
+//                 Mis Quince Años
+//             </span>
 
-            {/* Segundo Span - Texto visible con contorno oscuro */}
-            <span className="relative text-outline text-glow">Mis Quince Años</span>
-        </h1>
-    );
-}
+//             {/* Segundo Span - Texto visible con contorno oscuro */}
+//             <span className="relative text-outline text-glow">Mis Quince Años</span>
+//         </h1>
+//     );
+// }
--- src/components/main/PersonalDescription.tsx
+++ src/components/main/PersonalDescription.tsx
@@ -1,37 +1,47 @@
 // src/components/PersonalDescription.tsx
-import MisQuince from "../header/MisQuince";
-// import TextWithAnimation from "./TextWithAnimation";
+// import MisQuince from "../header/MisQuince";
+import TextWithAnimation from "./TextWithAnimation";
 
 const PersonalDescription = () => {
-    const text = `Tal y como salido de un cuento de hadas,
-                el sueño del libro de cuentos finalmente 
-                    se hace realidad.
-                    Yo, Alejandra Amaya junto a mi familia 
-                    requerimos el honor
-                    de su presencia en mi fiesta de 15 años.
-                    Con la bendición y el cuidado de mi familia 
-                    expresare mis
-                    sentimientos ante Dios al convertirme en toda 
-                    una señorita.`;
+    const personalDescription = `
+    Tal y como salido de un cuento de hadas,
+                  el sueño del libro de cuentos finalmente 
+                  se hace realidad.
+                  Yo, Alejandra Amaya 
+                  junto a mi familia requerimos el honor
+                  de su presencia en mi fiesta de 15 años.
+                  Con la bendición y el cuidado 
+                  de mi familia expresare mis
+                  sentimientos ante Dios al convertirme 
+                  en toda una señorita.
+                  `;
+    const myBirthday = `
+    Mis Quince Años
+                  `;
 
     return (
-        <div >
-            <section className="flex justify-center">
-                <span className="m-4 text-2xl text-center font-bold text-pink-300 text-shadow-soft">
-                    {text}
+        <div className="">
+
+            <section className="mb-8">
+                <span className="text-5lg font-bold text-pink-300 text-shadow-soft">
+                    <TextWithAnimation text={personalDescription} />
                 </span>
             </section>
-            <br />
-            {/* <section>
-                <TextWithAnimation text={text} />
-            </section> */}
-            <section className="flex justify-center">
-                <MisQuince />
+
+            <section>
+                <h1 className="inline-block font-decorative text-4xl font-semibold text-gray-200 italic text-italic-custom relative">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#c0c0c0] via-[#e0e0e0] to-[#c0c0c0] bg-[length:200%_auto] animate-shimmer absolute inset-0">
+                        <TextWithAnimation text={myBirthday} />
+                    </span>
+                    <span className="text-outline text-glow relative">
+                        <TextWithAnimation text={myBirthday} />
+                    </span>
+                </h1>
             </section>
 
         </div>
-
     );
+
 };
 
 export default PersonalDescription;
